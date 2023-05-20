6151774d28efda11ef25f5513ed0cecf74eaaddd
Neelotpal <neelotpal10@gmail.com>
2023-05-20 23:56:37 +0530

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'lamborghini357D',
+        'HOST':'localhost',
     }
 }
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -123,3 +127,8 @@ STATIC_ROOT=os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, "carzone/static"),
 ]
+
+#Media settings
+
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
+MEDIA_URL="/media/"
